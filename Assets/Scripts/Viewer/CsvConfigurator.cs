f28b92252de1123f7f413faec536e46fbb472542
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2021-07-25 00:41:23 +0900

    :sparkles: load csv

--- Assets/Scripts/Viewer/CsvConfigurator.cs
+++ Assets/Scripts/Viewer/CsvConfigurator.cs
@@ -1,29 +1,31 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using Share;
 using UniRx;
-using UnityEditor;
 using UnityEngine;
 
 namespace Viewer
 {
     public class CsvConfigurator : MonoBehaviour, IPointDataSupplier
     {
-        private readonly Subject<PackedMessage.IdentifiedPointArray> _supply
+        protected internal readonly Subject<PackedMessage.IdentifiedPointArray> Supply
             = new Subject<PackedMessage.IdentifiedPointArray>();
 
         public IObservable<PackedMessage.IdentifiedPointArray> OnReceivePointData()
         {
-            return _supply;
+            return Supply;
         }
 
         [SerializeField]
         protected internal PointRoot pointRoot;
     }
 
-    [CustomEditor(typeof(CsvConfigurator))]
+#if UNITY_EDITOR
+
+    [UnityEditor.CustomEditor(typeof(CsvConfigurator))]
     public class CsvConfiguratorEditor : UnityEditor.Editor
     {
         private CsvConfigurator configurator = null;
@@ -32,8 +34,11 @@ namespace Viewer
         {
             base.OnInspectorGUI();
 
-            EditorGUI.BeginDisabledGroup(!EditorApplication.isPlaying);
+            UnityEditor.EditorGUILayout.Space();
 
+            UnityEditor.EditorGUILayout.HelpBox("Save PointData to csv", UnityEditor.MessageType.Info);
+
+            UnityEditor.EditorGUI.BeginDisabledGroup(!UnityEditor.EditorApplication.isPlaying);
             if (GUILayout.Button(nameof(Save)))
             {
                 if (configurator == null)
@@ -42,23 +47,30 @@ namespace Viewer
                 }
                 Save().Forget();
             }
+            UnityEditor.EditorGUI.EndDisabledGroup();
+
+            UnityEditor.EditorGUILayout.Space();
+
+            UnityEditor.EditorGUILayout.HelpBox("Load PointData from csv", UnityEditor.MessageType.Info);
+
+            UnityEditor.EditorGUI.BeginDisabledGroup(!UnityEditor.EditorApplication.isPlaying);
             if (GUILayout.Button(nameof(Load)))
             {
                 if (configurator == null)
                 {
                     configurator = target as CsvConfigurator;
                 }
-                Load();
+                Load().Forget();
             }
-
-            EditorGUI.EndDisabledGroup();
+            UnityEditor.EditorGUI.EndDisabledGroup();
         }
 
         private const string ExtensionCsv = "csv";
 
         private async UniTaskVoid Save()
         {
-            var dirPath = EditorUtility.SaveFolderPanel(nameof(Save), Path.Combine(Application.dataPath, ExtensionCsv), ExtensionCsv);
+            var dirPath = UnityEditor.EditorUtility.SaveFolderPanel(
+                nameof(Save), Path.Combine(Application.dataPath, ExtensionCsv), ExtensionCsv);
             if (string.IsNullOrEmpty(dirPath))
             {
                 return;
@@ -90,12 +102,54 @@ namespace Viewer
                 {
                     Debug.LogError($"{pointSet.Time.ToString(PointSet.DateFormat)}");
                     Debug.LogException(e);
+                    return;
                 }
             }
+
+            Debug.Log($"Save {array.Length} to {dirPath}");
         }
 
         private async UniTaskVoid Load()
         {
+            var dirPath = UnityEditor.EditorUtility.OpenFolderPanel(
+                nameof(Load), Path.Combine(Application.dataPath, ExtensionCsv), ExtensionCsv);
+            if (string.IsNullOrEmpty(dirPath))
+            {
+                return;
+            }
+            var dirInfo = new DirectoryInfo(dirPath);
+            if (!dirInfo.Exists)
+            {
+                return;
+            }
+
+            await UniTask.SwitchToThreadPool();
+
+            var lineCache = new List<string>();
+
+            foreach (var fileInfo in dirInfo.EnumerateFiles())
+            {
+                if (!fileInfo.Exists || !string.Equals(fileInfo.Extension, $".{ExtensionCsv}"))
+                {
+                    continue;
+                }
+
+                using var reader = fileInfo.OpenText();
+                // ignore header
+                await reader.ReadLineAsync();
+
+                lineCache.Clear();
+                while (!reader.EndOfStream)
+                {
+                    var line = await reader.ReadLineAsync();
+                    lineCache.Add(line);
+                }
+                var info = CSVParser.FromCsv(lineCache.ToArray());
+                configurator.Supply.OnNext(info);
+            }
+
+            Debug.Log($"Load from {dirPath}");
         }
     }
+#endif
 }
